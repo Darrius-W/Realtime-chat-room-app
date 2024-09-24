804c27baaf8663555907bf635198de5614d0476b
Darrius-W <darriuswhite435@gmail.com>
2024-09-24 00:52:46 -0500

    test fix

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 import eventlet
-eventlet.monkey_patch()
+eventlet.monkey_patch(select=True, socket=True)
 from flask import Flask, session, request, jsonify, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room, send, emit
 from flask_session import Session
