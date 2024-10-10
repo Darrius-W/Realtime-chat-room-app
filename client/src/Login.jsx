4debbe1df8ae0ec820669fab3ea5482fac500b88
Darrius-W <darriuswhite435@gmail.com>
2024-10-09 20:12:31 -0500

    Updated login statements

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -17,10 +17,8 @@ export default function Login(){
 
 
     const handleLogin = async (event) => {
-        const data = { name: userName }
         event.preventDefault()
         try {
-            alert("here");
             const response = await axios.post('https://realtime-chat-room-app.onrender.com/LoginUser', { userName, userPassword }, { withCredentials: true })
                 /*.then(response => {
                     alert("here3");
@@ -30,11 +28,7 @@ export default function Login(){
                 .catch(error => alert("ERROrrr: Invalid Credentials"));*/
 
             //const response = await axios.post('http://localhost:5000/Login', { userName, userPassword }, { withCredentials: true });
-            alert("here2");
             if(response.status === 200){
-                alert("here3");
-                setMessage('Logged in successfully');
-                //setLoggedInUser(response.data.userName)
                 // Redirect to joinroom page
                 const data = { name: userName }
                 navigate("https://dw-realtime-chatroom-app.netlify.app/Joinroom", { state: data });
