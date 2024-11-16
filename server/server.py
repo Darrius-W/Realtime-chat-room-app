8f4074580a65fa13b0feb6f629bcc46885972420
Darrius-W <darriuswhite435@gmail.com>
2024-11-16 15:34:05 -0600

    test

--- server/server.py
+++ server/server.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, session, request, jsonify, send_from_directory
 from flask_socketio import SocketIO, join_room, leave_room, emit
 from flask_session import Session
